5d9f7d593987c2e44250d181325661a14aff3bc4
jonahlipsky <jonahlipsky@gmail.com>
2019-05-08 21:18:43 -0400

    Building out infrastructure of project

---
+++ frontend/components/root.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <h1>
+      Root Component
+    </h1>
+  )
+}
---
+++ frontend/ddm2.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configuredStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // let store;
+  // if(window.currentUser) {
+  //   const preloadedState = {
+  //     session: { id: window.currentUser.id },
+  //     entities: {
+  //       users: { [window.currentUser.id]: currentUser }
+  //     }
+  //   };
+  //   store = configuredStore(preloadedState);
+  //   delete window.currentUser;
+  // } else {
+  let store = configuredStore();
+  // }
+
+
+  debugger
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
